c8fa1be5e2a252753f2f7edf6170e1cbba60b962
Flyrik <flyrik8@gmail.com>
2024-06-12 11:02:09 +0200

    p

---
+++ vue-project/src/components/TrainDetail.vue
@@ -0,0 +1,23 @@
+<!-- src/components/TrainDetail.vue -->
+<template>
+    <div class="train-detail" v-if="train">
+      <h2>Détails du Train : {{ train.name }}</h2>
+      <p>ID du Train : {{ train.id }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "TrainDetail",
+    props: {
+      train: Object
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .train-detail {
+    padding: 20px;
+  }
+  </style>
+  
